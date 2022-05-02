7c17137237a15c4025c6ee8f5ad7ae54bb2ff798
frankdarkluo <2875902240@qq.com>
2022-05-01 23:55:43 -0600

    gpt3 & gpt-j-6B

--- output/splash.py
+++
@@ -1,7 +0,0 @@
-with open('gpt3-curie-001_500_1-0.txt', 'r', encoding='utf8') as f, \
-    open('gpt3-curie-001_1-0.txt','w',encoding='utf8') as of:
-        datas=f.readlines()
-        for idx, data in enumerate(datas):
-            if 'Generated:' in datas[idx]:
-                out_sent=datas[idx+1].strip().split('}')[0].lower()
-                of.write(out_sent+'\n')
